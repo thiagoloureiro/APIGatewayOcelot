e4ab33d02f347e41f20bb6d6817e5b65a17dfd0a
Thiago Loureiro <thiagoguaru@me.com>
2025-12-23 21:11:58 -0500

    test

--- APIGatewayOcelot/Program.cs
+++ APIGatewayOcelot/Program.cs
@@ -12,7 +12,7 @@ namespace APIGatewayOcelot
             new WebHostBuilder()
                 .UseKestrel(options =>
                 {
-                    options.ListenLocalhost(9999);
+                    options.ListenAnyIP(9999);
                 })
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
